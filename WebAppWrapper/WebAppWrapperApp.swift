3453477dbb51acb958852af7af1714fa835e0b07
CB2Moon <hideMYmask@outlook.com>
2025-01-19 11:46:12 +1000

    Initial Commit

---
+++ WebAppWrapper/WebAppWrapperApp.swift
@@ -0,0 +1,17 @@
+//
+//  WebAppWrapperApp.swift
+//  WebAppWrapper
+//
+//  Created by CB on 19/1/2025.
+//
+
+import SwiftUI
+
+@main
+struct WebAppWrapperApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ WebAppWrapperTests/WebAppWrapperTests.swift
@@ -0,0 +1,17 @@
+//
+//  WebAppWrapperTests.swift
+//  WebAppWrapperTests
+//
+//  Created by CB on 19/1/2025.
+//
+
+import Testing
+@testable import WebAppWrapper
+
+struct WebAppWrapperTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
